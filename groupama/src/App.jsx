e1940c927325385eb7a6319132a076ee699f03c1
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-08-23 18:45:28 +0200

    adds css for details component

--- groupama/src/App.jsx
+++ groupama/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import Signee from './components/Signee.jsx'
+import Details from './components/Details.jsx'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div id='root'>
       <Header />
       <Signee />
+      <Details />
     </ div>
   )
 }
