680008a5c29e8dd4dae470e6dedde5f89290a011
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-08-24 00:19:15 +0200

    adds more responsivity

--- groupama/src/components/Details.jsx
+++ groupama/src/components/Details.jsx
@@ -5,8 +5,10 @@ function Details() {
       <div className="Menu">
         <nav>Fizetendő tételek</nav>
         <nav>Eseti díj befizetése</nav>
+        <div className="CurrentPageContainer">
         <nav className="CurrentPage">Fizetési előzmények</nav>
-        <img src="./src/assets/rectangle.svg" alt="rectangle" className="rectangle" />
+        <img src="./src/assets/rectangle.svg"  className="rectangle" />
+        </div>
       </div>
 
       <div className="LetterHead">
@@ -24,11 +26,15 @@ function Details() {
             <div className="Card1Id">EPAYMT8...</div>
           </div>
           <div className="Card1Method">Bankkártya</div>
+          <div className="TransactionMiddle" id="Middle1">
           <div className="Card1Date">2019.06.25</div>
           <div className="CardAmount">5 000 Ft</div>
+          <div className="StatusContainer">
           <img src="./src/assets/ellipse.svg"  className="ellipse" />
           <div className="Card1Status">Folyamatban</div>
-          <div className="TransactionIcons">
+          </div>
+          </div>
+          <div className="TransactionIcons" id="Icons1">
             <img src="./src/assets/attack.svg"  className="AttachIcon" />
             <img src="./src/assets/arrow.svg"  className="Arrow" />
           </div>
@@ -40,12 +46,16 @@ function Details() {
             <div className="Card2Id">123456789</div>
           </div>
           <div className="Card2Method">Átutalás</div>
-          <div className="Card2Date">2020.01.15</div>
+          <div className="TransactionMiddle" id="Middle2">
+          <div className="Card2Date">2019.06.25</div>
           <div className="CardAmount">5 000 Ft</div>
+          <div className="StatusContainer">
           <img src="./src/assets/green.svg"  className="ellipse" />
           <div className="Card2Status">Beérkezett</div>
-          <div className="TransactionIcons">
-            <img src="./src/assets/attack.svg"  className="AttachIcon" />
+          </div>
+          </div>
+          
+          <div className="TransactionIcons" id="Icons2">
             <img src="./src/assets/arrow.svg"  className="Arrow" />
           </div>
         </div>
@@ -53,14 +63,18 @@ function Details() {
         <div className="Card3">
           <div className="TransactionPart">
             <img src="./src/assets/vector.svg"  className="vector" />
-            <div className="Card3Id">EPAYMT10...</div>
+            <div className="Card3Id">EPAYMT8...</div>
           </div>
           <div className="Card3Method">Bankártya</div>
+          <div className="TransactionMiddle" id="Middle3">
           <div className="Card3Date">2020.03.22</div>
           <div className="CardAmount">5 000 Ft</div>
+          <div className="StatusContainer">
           <img src="./src/assets/red.svg"  className="ellipse" />
           <div className="Card3Status">Sikertelen</div>
-          <div className="TransactionIcons">
+          </div>
+          </div>
+          <div className="TransactionIcons" id="Icons3">
             <img src="./src/assets/attack.svg"  className="AttachIcon" />
             <img src="./src/assets/arrow.svg"  className="Arrow" />
           </div>
@@ -69,14 +83,18 @@ function Details() {
         <div className="Card4">
           <div className="TransactionPart">
             <img src="./src/assets/vector.svg"  className="vector" />
-            <div className="Card4Id">EPAYMT11...</div>
+            <div className="Card4Id">EPAYMT8...</div>
           </div>
           <div className="Card4Method">Bankkártya</div>
-          <div className="Card4Date">2021.05.08</div>
+          <div className="TransactionMiddle" id="Middle4">
+          <div className="Card4Date">2019.06.25</div>
           <div className="CardAmount">5 000 Ft</div>
+          <div className="StatusContainer">
           <img src="./src/assets/green.svg"  className="ellipse" />
           <div className="Card4Status">Beérkezett</div>
-          <div className="TransactionIcons">
+          </div>
+          </div>
+          <div className="TransactionIcons" id="Icons4">
             <img src="./src/assets/attack.svg"  className="AttachIcon" />
             <img src="./src/assets/arrow.svg"  className="Arrow" />
           </div>
