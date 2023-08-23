442829baa75f46a47e9c53515d9569288bcc3272
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-08-23 19:16:53 +0200

    adds details

--- groupama/src/App.jsx
+++ groupama/src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css'
-import Header from './components/Header.jsx'
-import Signee from './components/Signee.jsx'
-import Details from './components/Details.jsx'
+import './App.css';
+import Header from './components/Header.jsx';
+import Signee from './components/Signee.jsx';
+import Details from './components/Details.jsx';
+import { Helmet } from 'react-helmet';
 
 function App() {
-
-
   return (
     <div id='root'>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Open+Sans:wght@300;400;500;600;700;800&family=Overpass:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
       <Header />
       <Signee />
       <Details />
-    </ div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
